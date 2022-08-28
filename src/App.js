4fddff3f4a0e4f3ea662018b8bc1de3b58a9a49f
Teo <106945447+Nambg1996@users.noreply.github.com>
2022-08-28 17:04:33 +0900

    change 8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>test lan 7</h1>
+      <h1>test lan 8</h1>
       </header>
     </div>
   );
