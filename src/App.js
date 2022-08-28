76c34746a55f661a635cf0a0435601a8f8d6adda
Teo <106945447+Nambg1996@users.noreply.github.com>
2022-08-28 15:52:23 +0900

    change h1 Test second

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>test lan 1</h1>
+      <h1>test lan 2</h1>
       </header>
     </div>
   );
